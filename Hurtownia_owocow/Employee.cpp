703ed47244eebcbf5f8b0b37d84b8add92b86554
vvszewczyk <0wojtek.szewczyk0@gmail.com>
2025-03-10 13:26:02 +0100

    Add considerReturn and realizeOrder Employee prompts

--- Hurtownia_owocow/Employee.cpp
+++ Hurtownia_owocow/Employee.cpp
@@ -1,13 +1,29 @@
 #include "Employee.h"
+#include "Order.h"
+#include <iostream>
 
-Employee::Employee(std::string l, std::string h) : Person(std::move(l), std::move(h)) {}
+Employee::Employee(std::string l, std::string h) : Person(std::move(l), std::move(h))
+{
+}
 
 void Employee::considerReturn()
 {
+    std::string orderID;
+    std::cout << "Podaj numer zamowienia do rozpatrzenia zwrotu: ";
+    std::cin >> orderID;
+    Order order;
+    order.updateState(getLogin(), orderID, "Zwrot");
+    std::cout << "Zamowienie " << orderID << " zosta³o oznaczone jako zwrot\n";
 }
 
 void Employee::realizeOrder()
 {
+    std::string orderID;
+    std::cout << "Podaj numer zamowienia do realizacji: ";
+    std::cin >> orderID;
+    Order order;
+    order.updateState(getLogin(), orderID, "Zrealizowane");
+    std::cout << "Zamowienie " << orderID << " zostalo zrealizowane\n";
 }
 
 void Employee::updateFruit()
